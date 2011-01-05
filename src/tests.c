8aa4d621a18adf5d94e86d0fb68322de83687aa0
Ian Sproates <sproates@gmail.com>
2011-01-05 07:53:17 +0000

    Moved tests from main to test script

--- src/tests.c
+++ src/tests.c
@@ -23,7 +23,34 @@ int string_tests(void) {
 }
 
 int lexer_tests(void) {
+  FILE * script_file;
+  lexer * lex;
+  token * tok;
+
   printf("Running lexer tests...\n");
+
+  if(0 == (script_file = fopen("test.oca", "r"))) {
+    printf("Could not open source file for reading.\n");
+    return 1;
+  }
+  if(0 == (lex = lexer_new(script_file))) {
+    printf("Unable to start lexing\n");
+    return 1;
+  }
+  while(1) {
+    tok = lexer_next_token(lex);
+    if(0 == tok || TOKEN_EOF == tok->type) {
+      token_delete(tok);
+      break;
+    }
+    token_delete(tok);
+  }
+  if(EOF == fclose(script_file)) {
+    printf("Failed to close file handle on source file.\n");
+    return 1;
+  }
+  lexer_delete(lex);
+
   printf("Completed lexer tests\n");
   return 0;
 }
