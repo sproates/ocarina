98a8ff9053021d53160e08b08154afaa38d4b218
Ian Sproates <sproates@gmail.com>
2011-01-10 15:59:51 +0000

    script stuff

--- src/script.c
+++ src/script.c
@@ -1,5 +1,12 @@
 #include "script.h"
 #include "memory.h"
+#include "string.h"
+
+/* private function prototypes */
+
+script_impl * _scr_impl_new(void);
+void _scr_impl_del(script_impl * impl);
+void _str_scr_del(script * s);
 
 /* public function definitions */
 
@@ -10,13 +17,30 @@
  *
  * @return A pointer to a script on success, zero on failure.
  */
-script * scr_new(script_type type) {
+script * scr_new(script_type type, char * source) {
   script * scr;
-  if(0 == (scr = mem_alloc(sizeof(script)))) {
-    return 0;
+  if(0 == (scr = mem_alloc(sizeof(script))) ||
+     0 == (scr->impl = _scr_impl_new())) {
+    goto error;
   }
   scr->type = type;
+  switch(type) {
+    case SCR_FILE:
+      scr->impl->f->filename = source;
+      break;
+    case SCR_STR:
+      scr->impl->s->s = str_new(source);
+      if(0 == scr->impl->s->s) {
+        goto error;
+      }
+      break;
+    default:
+      goto error;
+  }
   return scr;
+error:
+  scr_del(scr);
+  return 0;
 }
 
 /**
@@ -26,6 +50,33 @@ script * scr_new(script_type type) {
  */
 void scr_del(script * scr) {
   if(0 != scr) {
+    _scr_impl_del(scr->impl);
     mem_free(scr);
+  }
+}
+
+/* private function definitions */
+
+/**
+ * Create a new script_impl instance.
+ *
+ * @return A pointer to a script_impl on success, zero on failure.
+ */
+script_impl * _scr_impl_new(void) {
+  script_impl * impl;
+  if(0 == (impl = mem_alloc(sizeof(script_impl)))) {
+    return 0;
+  }
+  return impl;
+}
+
+/**
+ * Destructs a script_impl.
+ *
+ * @param scr The script_impl to delete.
+ */
+void _scr_impl_del(script_impl * impl)  {
+  if(0 != impl) {
+    mem_free(impl);
   }
 }
--- src/script.h
+++ src/script.h
@@ -1,20 +1,41 @@
 #ifndef SCRIPT_H
 #define SCRIPT_H
 
+#include "string.h"
+#include <stdio.h>
+
 /* public structure definitions */
+
+typedef struct {
+  char * filename;
+  FILE * f;
+  char * buf;
+  int buf_size;
+  int max_buf;
+} file_script;
+
+typedef struct {
+  string * s;
+} string_script;
 
 typedef enum {
   SCR_FILE,
   SCR_STR
 } script_type;
 
+typedef union {
+  string_script * s;
+  file_script * f;
+} script_impl;
+
 typedef struct {
   script_type type;
+  script_impl * impl;
 } script;
 
 /* public function prototypes */
 
-script * scr_new(script_type type);
+script * scr_new(script_type type, char * source);
 void scr_del(script * scr);
 
 #endif
