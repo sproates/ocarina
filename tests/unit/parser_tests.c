506b74b95cc953408a7955e1f6b28c09cc3e89a5
Ian Sproates <sproates@gmail.com>
2011-02-03 11:40:11 +0000

    Renamed tests

---
+++ tests/unit/parser_tests.c
@@ -0,0 +1,45 @@
+/* Copyright 2011 Ian Sproates <ian@sproates.net> */
+
+/**
+ * @file parser_tests.c
+ * Parser unit tests.
+ */
+
+#include <stdio.h>
+#include "unit_tests.h"
+#include "parser.h"
+#include "lexer.h"
+
+/* public function prototypes */
+
+/**
+ * Run the tests for the parser functions.
+ */
+void parser_tests(void);
+
+/* public function definitions */
+
+void parser_tests(void) {
+  parser * par;
+  lexer * lex;
+
+  printf("Running parser tests...\n");
+
+  printf("Created script.\n");
+  if(0 == (lex = lex_new_file("test.oca"))) {
+    printf("Unable to create lexer.\n");
+    return;
+  }
+  printf("Created lexer.\n");
+  printf("par_new()\n");
+  if(0 == (par = par_new(lex))) {
+    printf("Failed to create parser\n");
+    test_fail();
+    return;
+  }
+  test_pass();
+  lex_del(lex);
+  par_del(par);
+
+  printf("Completed parser tests\n");
+}
---
+++ tests/unit/script_tests.c
@@ -0,0 +1,72 @@
+/* Copyright 2011 Ian Sproates <ian@sproates.net> */
+
+/**
+ * @file script_tests.c
+ * Script unit tests.
+ */
+
+#include "script.h"
+#include "unit_tests.h"
+
+/* public function prototypes */
+
+/**
+ * Run the tests for the script functions.
+ */
+void script_tests(void);
+
+/* public function definitions */
+
+void script_tests(void) {
+  script * s;
+  char c;
+
+  printf("Running script tests...\n");
+
+  printf("scr_new() (file type)\n");
+  if(0 == (s = scr_new(SCR_FILE, "test.oca"))) {
+    printf("Failed to create a new script\n");
+    test_fail();
+    return;
+  }
+  test_pass();
+
+  printf("scr_nextr()\n");
+  if('1' != (c = scr_next(s))) {
+    printf("scr_next() returned the wrong char\n");
+    test_fail();
+  }
+  test_pass();
+  scr_del(s);
+
+  printf("scr_new() (string type)\n");
+  if(0 == (s = scr_new(SCR_STR, "abc"))) {
+    printf("Failed to create a new script\n");
+    test_fail();
+    return;
+  }
+  test_pass();
+
+  printf("scr_next()\n");
+  if('a' != (c = scr_next(s))) {
+    printf("scr_next() returned the wrong char\n");
+    test_fail();
+  }
+  test_pass();
+  if('b' != (c = scr_next(s))) {
+    printf("scr_next() returned the wrong char\n");
+    test_fail();
+  }
+  test_pass();
+  if('c' != (c = scr_next(s))) {
+    printf("scr_next() returned the wrong char\n");
+    test_fail();
+  }
+  test_pass();
+  if(EOF != (c = scr_next(s))) {
+    printf("scr_next() returned the wrong char\n");
+    test_fail();
+  }
+  test_pass();
+  scr_del(s);
+}
