c1bcac7851ba64e2fe54a7796804d07560a2d1e5
Ian Sproates <sproates@gmail.com>
2011-02-07 17:41:31 +0000

    Added beginnings of performance test suite

---
+++ tests/performance/performance_tests.c
@@ -0,0 +1,19 @@
+/* Copyright 2011 Ian Sproates <ian@sproates.net> */
+
+/**
+ * @file performance_tests.c
+ * A really basic performance test suite.
+ */
+
+#include <stdio.h>
+#include "performance_tests.h"
+
+/* private function prototypes */
+
+/* public function definitions */
+
+int main(void) {
+  return 0;
+}
+
+/* private function definitions */
---
+++ tests/performance/performance_tests.h
@@ -0,0 +1,20 @@
+/* Copyright 2011 Ian Sproates <ian@sproates.net> */
+
+#ifndef PERFORMANCE_TESTS_H
+#define PERFORMANCE_TESTS_H
+
+/**
+ * @file performance_tests.h
+ * A really basic performance test suite.
+ */
+
+/* public function prototypes */
+
+/**
+ * Entry point for the test suite.
+ *
+ * @return Zero on success, non-zero if at least one test fails.
+ */
+int main(void);
+
+#endif
