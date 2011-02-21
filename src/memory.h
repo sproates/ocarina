d57b12e8eb2bc54a70ffae9324118ce1176f69d9
Ian Sproates <sproates@gmail.com>
2011-02-21 13:25:00 +0000

    Modified comments

--- src/memory.h
+++ src/memory.h
@@ -10,7 +10,8 @@
  * At the moment these are just wrappers around malloc, free and realloc.
  *
  * They are useful for debugging purposes. When there is a memory issue, we can
- * drop some logging into them.
+ * drop some logging into them, or we can make malloc intermittently fail to
+ * simulate out of memory errors.
 */
 
 #include <stdlib.h>
